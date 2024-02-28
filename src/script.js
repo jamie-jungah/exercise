79c9b11d8ccd640ef623f0ffc18ef29ec679cc2c
jamie-jungah <wjddk718@gmail.com>
2024-02-28 20:50:48 +0900

    3D Text

--- src/script.js
+++ src/script.js
@@ -1,192 +1,80 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import GUI from 'lil-gui';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
-
-/**
- * Debug
- */
-const gui = new GUI();
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
 /**
  * Base
  */
+// Debug
+const gui = new GUI();
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
 // Scene
 const scene = new THREE.Scene();
 
+// Axes helper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-
-const doorColorTexture = textureLoader.load('./textures/door/color.jpg');
-const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg');
-const doorAmbientOcclusionTexture = textureLoader.load(
-  './textures/door/ambientOcclusion.jpg'
-);
-const doorHeightTexture = textureLoader.load('./textures/door/height.jpg');
-const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg');
-const doorMetalnessTexture = textureLoader.load(
-  './textures/door/metalness.jpg'
-);
-const doorRoughnessTexture = textureLoader.load(
-  './textures/door/roughness.jpg'
-);
-const matcapTexture = textureLoader.load('./textures/matcaps/8.png');
-const gradientTexture = textureLoader.load('./textures/gradients/5.jpg');
-
-doorColorTexture.colorSpace = THREE.SRGBColorSpace;
+const matcapTexture = textureLoader.load('/textures/matcaps/7.png');
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
- * Objects
+ * Fonts
  */
-// MeshBasicMaterial
-// const material = new THREE.MeshBasicMaterial();
-// material.map = doorColorTexture;
-// material.color = new THREE.Color(0xff0000);
-// material.wireframe = true;
-// material.transparent = true;
-// material.opacity = 0.5;
-// material.alphaMap = doorAlphaTexture;
-// material.side = THREE.DoubleSide;
+const fontLoader = new FontLoader();
+fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+  const textGeometry = new TextGeometry('Jung-ah Choi', {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 5,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4,
+  });
 
-// MeshNormalMaterial
-// const material = new THREE.MeshNormalMaterial();
-// material.flatShading = true;
+  // textGeometry.computeBoundingBox();
+  // textGeometry.translate(
+  //   -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //   -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //   -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+  // );
+  textGeometry.center();
 
-// MeshMatcapMaterial
-// const material = new THREE.MeshMatcapMaterial();
-// material.matcap = matcapTexture;
+  const material = new THREE.MeshMatcapMaterial({
+    matcap: matcapTexture,
+  });
+  const text = new THREE.Mesh(textGeometry, material);
+  scene.add(text);
 
-// MeshDepthMaterial
-// const material = new THREE.MeshDepthMaterial();
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
 
-// MeshLamberMaterial
-// const material = new THREE.MeshLambertMaterial();
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
 
-// MeshPhongMaterial
-// const material = new THREE.MeshPhongMaterial();
-// material.shininess = 100;
-// material.specular = new THREE.Color(0x1188ff);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
 
-// MeshToonMaterial
-// const material = new THREE.MeshToonMaterial();
-// gradientTexture.minFilter = THREE.NearestFilter;
-// gradientTexture.magFilter = THREE.NearestFilter;
-// gradientTexture.generateMipmaps = false;
-// material.gradientMap = gradientTexture;
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
 
-// MeshStandardMaterial
-// const material = new THREE.MeshStandardMaterial();
-// material.metalness = 0.7;
-// material.roughness = 0.2;
-// material.map = doorColorTexture;
-// material.aomap = doorAmbientOcclusionTexture;
-// material.aoMapIntensity = 1;
-// material.displacementMap = doorHeightTexture;
-// material.displacementScale = 0.1;
-// material.metalnessMap = doorMetalnessTexture;
-// material.roughnessMap = doorRoughnessTexture;
-// material.normalMap = doorNormalTexture;
-// material.normalScale.set(0.5, 0.5);
-// material.transparent = true;
-// material.alphaMap = doorAlphaTexture;
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
 
-// gui.add(material, 'metalness').min(0).max(1).step(0.0001);
-// gui.add(material, 'roughness').min(0).max(1).step(0.0001);
-
-// MeshPhysicalMaterial
-const material = new THREE.MeshPhysicalMaterial();
-material.metalness = 0;
-material.roughness = 0;
-// material.map = doorColorTexture;
-// material.aomap = doorAmbientOcclusionTexture;
-// material.aoMapIntensity = 1;
-// material.displacementMap = doorHeightTexture;
-// material.displacementScale = 0.1;
-// material.metalnessMap = doorMetalnessTexture;
-// material.roughnessMap = doorRoughnessTexture;
-// material.normalMap = doorNormalTexture;
-// material.normalScale.set(0.5, 0.5);
-// material.transparent = true;
-// material.alphaMap = doorAlphaTexture;
-
-gui.add(material, 'metalness').min(0).max(1).step(0.0001);
-gui.add(material, 'roughness').min(0).max(1).step(0.0001);
-
-// Clearcoat
-// material.clearcoat = 1;
-// material.clearcoatRoughness = 0;
-
-// gui.add(material, 'clearcoat').min(0).max(1).step(0.0001);
-// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001);
-
-// Sheen
-// material.sheen = 1;
-// material.sheenRoughness = 0.25;
-// material.sheenColor.set(1, 1, 1);
-
-// gui.add(material, 'sheen').min(0).max(1).step(0.0001);
-// gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001);
-// gui.addColor(material, 'sheenColor');
-
-// Iridescence
-// material.iridescence = 1;
-// material.iridescenceIOR = 1;
-// material.iridescenceThicknessRange = [100, 800];
-
-// gui.add(material, 'iridescence').min(0).max(2.333).step(0.0001);
-// gui.add(material, 'iridescenceIOR').min(0).max(2.333).step(0.0001);
-// gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1);
-// gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1);
-
-// Transmission
-material.transmission = 1;
-material.ior = 1.5;
-material.thickness = 0.5;
-
-gui.add(material, 'transmission').min(0).max(1).step(0.0001);
-gui.add(material, 'ior').min(1).max(10).step(0.0001);
-gui.add(material, 'thickness').min(0).max(1).step(0.0001);
-
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
-sphere.position.x = -1.5;
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
-  material
-);
-torus.position.x = 1.5;
-
-scene.add(sphere, plane, torus);
-
-/**
- * Lights
- */
-// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-// scene.add(ambientLight);
-
-// const pointLight = new THREE.PointLight(0xffffff, 30);
-// pointLight.position.x = 2;
-// pointLight.position.y = 3;
-// pointLight.position.z = 4;
-// scene.add(pointLight);
-
-/**
- * Environment map
- */
-const rgbeLoader = new RGBELoader();
-rgbeLoader.load('./textures/environmentMap/2k.hdr', (environmentMap) => {
-  environmentMap.mapping = THREE.EquirectangularReflectionMapping;
-
-  scene.background = environmentMap;
-  scene.environment = environmentMap;
+    scene.add(donut);
+  }
 });
 
 /**
@@ -246,15 +134,6 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update objects
-  sphere.rotation.y = 0.1 * elapsedTime;
-  plane.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
-
-  sphere.rotation.x = -0.15 * elapsedTime;
-  plane.rotation.x = -0.15 * elapsedTime;
-  torus.rotation.x = -0.15 * elapsedTime;
 
   // Update controls
   controls.update();
